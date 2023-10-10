40d8a7f334245fd7bfd23d78ab6bdc1dfa0471d1
Ajey <ajeysrini@gmail.com>
2023-10-10 20:58:18 +0530

    build issue

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -18,32 +18,19 @@ const Header = () => {
       className="w-1/2 flex flex-row justify-end items-center nav"
       style={{ padding: "5px" }}
     >
-      <a
-        className="a-1 flex items-center px-3"
-        role="button"
-        style={{ padding: "5px" }}
-      >
+      
         <i className="fas fa-bell fa-fw"></i>
         <span className="badge badge-danger badge-counter ml-1">3+</span>
-      </a>
-      <a
-        className="a-1 flex items-center px-3"
-        role="button"
-        style={{ padding: "5px" }}
-      >
+      
         <i className="fas fa-envelope fa-fw"></i>
         <span className="badge badge-danger badge-counter ml-2">7</span>
-      </a>
+      
     </div>
     <div style={{ padding: "5px" }}>
-      <a
-        className="a-1 flex items-center px-3"
-        role="button"
-        style={{ display: "flex" }}
-      >
+     
         <span  style={{padding: "4px"}}>Ajey Srinivasu</span>
         <img className="img-profile rounded-full " style={{width:"28px",height:"28px",padding: "6px"}} src="src/assets/profile.svg"  alt='.' />
-      </a>
+      
     </div>
     </div>
     );
